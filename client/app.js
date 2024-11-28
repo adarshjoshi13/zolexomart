0017b7c70ed686a5d4adfaa3ce7fa1f472b953a0
ajay kumar <ajaykumarakraj1234@gmail.comr>
2024-11-28 14:29:03 +0530

    blog management added

--- client/app.js
+++ client/app.js
@@ -1,22 +1,22 @@
-const express = require('express')
+const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const ApiRoutes = require('./src/routes/api')
-const HomeRoutes = require('./src/routes/home')
+const axios = require('axios'); // Import Axios
+const ApiRoutes = require('./src/routes/api');
+const HomeRoutes = require('./src/routes/home');
 const { Connection, Services } = require('./src/models');
-const app = express()
+const app = express();
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    // origin: ['119.18.54.60', 'https://zolexomart.in'],  
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -34,7 +34,7 @@ app.use(async (req, res, next) => {
         const HomePageServices = await Services.findAll({
             raw: true,
             where: { status: 1 },
-            attributes: ['id', 'serviceName', 'subImageUrl', 'mainImageUrl', 'subDescription', 'route']
+            attributes: ['id', 'serviceName', 'subImageUrl', 'mainImageUrl', 'subDescription', 'route'],
         });
 
         // Make the services available in all views
@@ -48,6 +48,40 @@ app.use(async (req, res, next) => {
 
 app.use('/', HomeRoutes);
 app.use('/api', ApiRoutes);
+
+// Add a new route for the blog
+app.get('/blog', async (req, res) => {
+    try {
+        const response = await axios.get('https://public-api.wordpress.com/wp/v2/sites/zolexomartforblog.wordpress.com/posts');
+        const posts = response.data; // Get blog posts
+
+        // console.log('check posts', JSON.stringify(posts, null, 2));
+
+        res.render('blog', { posts: posts });
+    } catch (error) {
+        console.error('Error fetching blog posts:', error);
+        res.status(500).send('Unable to fetch blog posts.');
+    }
+});
+
+// Add a new route for the blog
+app.get('/blog/:id', async (req, res) => {
+    try {
+        const blogId = req.params.id;
+        const siteName = 'zolexomartforblog.wordpress.com'; // Replace with your WordPress site name
+        const response = await axios.get(`https://public-api.wordpress.com/wp/v2/sites/${siteName}/posts/${blogId}`);
+        
+        const post = response.data; // Get the specific blog post
+
+        // console.log('Blog post details:', JSON.stringify(post, null, 2));
+
+        res.render('specific-blog.pug', { post: post });
+    } catch (error) {
+        console.error('Error fetching the blog post:', error.response?.data || error.message);
+        res.status(500).send('Unable to fetch the blog post.');
+    }
+});
+
 
 // Start the server
 Connection().then(() => {
